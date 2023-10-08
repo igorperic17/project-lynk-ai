3f999bbd72c707f670d3d50956ad0fc382af1104
Igor Peric <igor@Igors-MacBook-Pro.local>
2023-10-08 02:49:12 +0200

    connected to supabase

---
+++ pages/api/add_project.ts
@@ -0,0 +1,9 @@
+// export default async (req, res) => {
+//     if (req.method === 'POST') {
+//       const bio = req.body.bio;
+//       // TODO: Process the bio and fetch matching projects
+//       res.status(200).json({ success: true });
+//     } else {
+//       res.status(405).json({ error: 'Method not allowed' });  // Only allow POST
+//     }
+//   };
---
+++ pages/api/match.ts
@@ -0,0 +1,170 @@
+import { createClient } from "@supabase/supabase-js";
+import { codeBlock } from "common-tags";
+import GPT3Tokenizer from "gpt3-tokenizer";
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+const openAiKey = process.env.OPENAI_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+const config = new Configuration({
+  apiKey: openAiKey,
+});
+const openai = new OpenAIApi(config);
+
+export const runtime = "edge";
+
+type ResponseData = {
+  error: string
+  data: string
+}
+
+type RequestBody = {
+  prompt: string
+}
+
+export default async function handler(req: Request, res: Response) {
+
+  const { prompt: query } = await req.json();
+  try {
+    if (!openAiKey) {
+      throw new Error("Missing environment variable OPENAI_KEY");
+    }
+    if (!supabaseUrl) {
+      throw new Error("Missing environment variable NEXT_PUBLIC_SUPABASE_URL");
+    }
+    
+    if (!supabaseKey) {
+      throw new Error("Missing environment variable NEXT_PUBLIC_SUPABASE_KEY");
+    }
+    
+    console.log(req);
+    console.log(query);
+
+    if (!query) {
+      throw new Error("Missing query in request data");
+    }
+
+    const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+    const sanitizedQuery = query.trim();
+    const moderationResponse = await openai
+      .createModeration({ input: sanitizedQuery })
+      .then((modRes) => modRes.json());
+
+    const [results] = moderationResponse.results;
+
+    if (results.flagged) {
+      throw new Error("Flagged content", 
+      // {
+      //   flagged: true,
+      //   categories: results.categories,
+      // });
+      )
+    }
+
+    const embeddingResponse = await openai.createEmbedding({
+      model: "text-embedding-ada-002",
+      input: sanitizedQuery.replaceAll("\n", " "),
+    });
+
+    if (embeddingResponse.status !== 200) {
+      throw new Error(
+        "Failed to create embedding for question",
+        // embeddingResponse,
+      );
+    }
+
+    const {
+      data: [{ embedding }],
+    } = await embeddingResponse.json();
+
+    // const { error: matchError, data: pageSections } = await supabaseClient.rpc(
+    //   "match_page_sections_lynk",
+    //   {
+    //     embedding,
+    //     match_threshold: 0.78,
+    //     match_count: 10,
+    //     min_content_length: 50,
+    //   },
+    // );
+
+    // if (matchError) {
+    //   console.log(matchError);
+    //   throw new Error("Failed to match page sections ${matchError}");
+    // }
+
+    // const tokenizer = new GPT3Tokenizer({ type: "gpt3" });
+    // let tokenCount = 0;
+    // let contextText = "";
+
+    // for (let i = 0; i < pageSections.length; i++) {
+    //   const pageSection = pageSections[i];
+    //   const content = pageSection.content;
+    //   const encoded = tokenizer.encode(content);
+    //   tokenCount += encoded.text.length;
+
+    //   if (tokenCount >= 1500) {
+    //     break;
+    //   }
+
+    //   contextText += `${content.trim()}\n---\n`;
+    // }
+
+    // const prompt = codeBlock`
+    //   ${`You know all the scientists in the world and help them find each other. Please use context information to answer. If you are unsure - answer with "I don't know".\n\n`}
+
+    //   Context sections:
+    //   ${contextText}
+
+    //   Question: """
+    //   ${sanitizedQuery}
+    //   """
+
+    //   Answer as markdown:
+    // `;
+
+    // const chatMessage: ChatCompletionRequestMessage = {
+    //   role: "user",
+    //   content: prompt,
+    // };
+
+    // const response = await openai.createChatCompletion({
+    //   model: "gpt-3.5-turbo",
+    //   messages: [chatMessage],
+    //   max_tokens: 512,
+    //   temperature: 0,
+    //   stream: true,
+    // });
+
+    // console.log(res);
+    // console.log(response);
+
+    // if (!response.ok) {
+    //   const err = await response.json();
+    //   return new Response(JSON.stringify(
+    //     { error: err.message, data: err.data }
+    //   ));
+    // }
+
+    // const stream = OpenAIStream(response);
+    // return new StreamingTextResponse(stream);
+
+    let { data: projects, error } = await supabaseClient
+      .from('projects')
+      .select('name')
+    
+    console.log(projects);
+    
+    return new Response(JSON.stringify(
+      { status: 400, error: null, data: projects }
+    ))
+
+  } catch (err: any) {
+    console.log(err);
+    return new Response(JSON.stringify(
+      { status: 400, error: err.message, data: err.data }
+    ))
+  }
+}
